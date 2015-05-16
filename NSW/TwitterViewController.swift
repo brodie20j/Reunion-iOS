a073fa7e8559f48f75e0971bcf8bfb73d2b73e6d
Jonathan Brodie <brodiej@carleton.edu>
2015-05-16 12:38:19 -0500

    added blank template for twitter page

---
+++ NSW/TwitterViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TwitterViewController.swift
+//  Carleton Reunion
+//
+//  Created by Jonathan Brodie on 5/16/15.
+//  Copyright (c) 2015 BTIN. All rights reserved.
+//
+//  View Controller for the Twitter feed
+
+import UIKit
+
+class TwitterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
